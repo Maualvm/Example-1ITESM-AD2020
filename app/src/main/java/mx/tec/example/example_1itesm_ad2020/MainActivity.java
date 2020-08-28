df77428ed5d3d4107ce4a03a89d2a151d46064dd
maualvm@gmail.com <maualvm@gmail.com>
2020-08-27 20:02:39 -0500

    Initial commit

---
+++ app/src/main/java/mx/tec/example/example_1itesm_ad2020/MainActivity.java
@@ -0,0 +1,24 @@
+package mx.tec.example.example_1itesm_ad2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button myButton;
+    TextView myTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myButton = findViewById(R.id.button1);
+        myTextView = findViewById(R.id.txtview1);
+
+        myTextView.setText("Goodbye class!");
+    }
+}
